cbecc91f0daca9badbec26e79d62b6478a5cdb0d
Szabolcs Bazil Papp <papp.szabolcs.bazil@gmail.com>
2025-12-23 14:23:17 +0100

    define db schema, add openapi spec skeleton

---
+++ backend/src/main/java/org/revizit/rest/model/AuthenticationResponse.java
@@ -0,0 +1,83 @@
+package org.revizit.rest.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import org.springframework.lang.Nullable;
+import java.time.OffsetDateTime;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import jakarta.annotation.Generated;
+
+/**
+ * AuthenticationResponse
+ */
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", comments = "Generator version: 7.17.0")
+public class AuthenticationResponse {
+
+  private @Nullable String token;
+
+  public AuthenticationResponse token(@Nullable String token) {
+    this.token = token;
+    return this;
+  }
+
+  /**
+   * Get token
+   * @return token
+   */
+  
+  @Schema(name = "token", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("token")
+  public @Nullable String getToken() {
+    return token;
+  }
+
+  public void setToken(@Nullable String token) {
+    this.token = token;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    AuthenticationResponse authenticationResponse = (AuthenticationResponse) o;
+    return Objects.equals(this.token, authenticationResponse.token);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(token);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class AuthenticationResponse {\n");
+    sb.append("    token: ").append(toIndentedString(token)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
--- backend/src/main/resources/db/changelog/1/1-initial.sql
+++ backend/src/main/resources/db/changelog/1/1-initial.sql
@@ -1,1 +1,62 @@
 --liquibase formatted sql
+
+--changeset bazil:1
+create table if not exists user_account
+(
+    id         serial     not null primary key,
+    username   text       not null,                 -- username used for authentication
+    mail_addr  text       not null default '',      -- email address
+    user_pw    text       not null,                 -- encrypted password
+    user_role  varchar(5) not null default 'PLAIN', -- possible values: PLAIN, ADMIN
+    inactive   boolean    not null default false,
+    reg_token  uuid,                                -- token for registration and password recovery
+    token_exp  timestamp without time zone,         -- expiration timestamp for the token, if any
+    profile_id int
+);
+
+--changeset bazil:2
+create type report_type as enum (
+    'BALLOON_CHANGE',
+    'SET_PERCENTAGE',
+    'BALLOON_REFILL'
+    );
+
+--changeset bazil:3
+create table water_flavour
+(
+    id   serial primary key,
+    name text not null unique
+);
+
+--changeset bazil:4
+create table water_report
+(
+    id          serial primary key,
+    kind        report_type                 not null,
+    val         int,
+    reported_at timestamp without time zone not null default now(),
+    reported_by int                         not null references user_account,
+    approved_at timestamp without time zone,
+    approved_by int references user_account
+);
+
+--changeset bazil:5
+create table water_state
+(
+    id         serial primary key,
+    empty_cnt  int                         not null default 0,
+    full_cnt   int                         not null default 0,
+    curr_pct   int                         not null,
+    curr_flav  int references water_flavour,
+    created_at timestamp without time zone not null default now(),
+    report     int references water_report
+);
+
+--changeset bazil:6
+create table flavour_request
+(
+    id           serial primary key,
+    name         text                        not null,
+    submitted_at timestamp without time zone not null default now(),
+    submitted_by int                         not null references user_account
+);
