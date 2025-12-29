3f0593faae1a74920524cfb3b7578e31d7a0a9de
Szabolcs Bazil Papp <papp.szabolcs.bazil@gmail.com>
2025-12-29 16:25:21 +0100

    change to lazy loading routes to decrease chunk size

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,20 +1,31 @@
 import {Routes} from '@angular/router';
 import {Home} from './view/home';
-import {Login} from './view/login';
-import {Settings} from './view/settings';
-import {Statistics} from './view/statistics';
-import {Reports} from './view/reports';
 import {AUTH_GUARD} from './auth/auth';
-import {CreateReport} from './view/create.report';
 
 export const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: Home},
-  {path: 'login', component: Login},
-  {path: 'create-report', component: CreateReport, canActivate: [AUTH_GUARD]},
-  {path: 'settings', component: Settings, canActivate: [AUTH_GUARD]},
-  {path: 'stats', component: Statistics, canActivate: [AUTH_GUARD]},
-  {path: 'reports', component: Reports, canActivate: [AUTH_GUARD]},
+  {path: 'login', loadComponent: () => import('./view/login').then(m => m.Login)},
+  {
+    path: 'create-report',
+    loadComponent: () => import('./view/create.report').then(m => m.CreateReport),
+    canActivate: [AUTH_GUARD]
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./view/settings').then(m => m.Settings),
+    canActivate: [AUTH_GUARD]
+  },
+  {
+    path: 'stats',
+    loadComponent: () => import('./view/statistics').then(m => m.Statistics),
+    canActivate: [AUTH_GUARD]
+  },
+  {
+    path: 'reports',
+    loadComponent: () => import('./view/reports').then(m => m.Reports),
+    canActivate: [AUTH_GUARD]
+  },
 
   {path: '**', redirectTo: '/home'}
 
