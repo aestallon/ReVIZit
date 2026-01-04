df1642052c22c5d8bc6f4d3c00cf64e5c4b7ae94
Szabolcs Bazil Papp <papp.szabolcs.bazil@gmail.com>
2026-01-04 14:38:56 +0100

    add logback config, clean up date rendering, set default user props

--- backend/src/main/java/org/revizit/RevizitApplication.java
+++ backend/src/main/java/org/revizit/RevizitApplication.java
@@ -15,9 +15,11 @@ import org.revizit.rest.model.WaterFlavourDto;
 import org.revizit.rest.model.WaterStateDto;
 import org.revizit.service.UserService;
 import org.revizit.service.WaterService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -44,25 +46,34 @@ public class RevizitApplication {
 
   @Bean
   CommandLineRunner runner(UserAccountRepository userAccountRepository,
-                           PasswordEncoder passwordEncoder) {
-    return _ -> userAccountRepository
-        .findByUsername("foo")
-        .ifPresentOrElse(
-            it -> log.info("User {} is available", it.getUsername()),
-            () -> {
-              final var user = new UserAccount();
-              user.setUserPw(passwordEncoder.encode("asd"));
-              user.setUsername("foo");
-              user.setMailAddr("foo@bar.com");
-              user.setUserRole("ADMIN");
-              user.setInactive(false);
+                           PasswordEncoder passwordEncoder,
+                           @Value("${revizit.default-username:}") String username,
+                           @Value("${revizit.default-password:}") String password) {
+    return _ -> {
+      if (username == null || username.isBlank() || password == null || password.isBlank()) {
+        log.info("No default username or password provided");
+        return;
+      }
 
-              final var profile = new UserProfile();
-              profile.setDisplayName("Foo");
-              user.setProfile(profile);
-              userAccountRepository.save(user);
-              log.info("User {} is created", user.getUsername());
-            });
+      userAccountRepository
+          .findByUsername(username)
+          .ifPresentOrElse(
+              it -> log.info("User {} is available", it.getUsername()),
+              () -> {
+                final var user = new UserAccount();
+                user.setUserPw(passwordEncoder.encode(password));
+                user.setUsername(username);
+                user.setMailAddr("");
+                user.setUserRole(UserService.ROLE_ADMIN);
+                user.setInactive(false);
+
+                final var profile = new UserProfile();
+                profile.setDisplayName(username);
+                user.setProfile(profile);
+                userAccountRepository.save(user);
+                log.info("User {} is created", user.getUsername());
+              });
+    };
   }
 
 }
